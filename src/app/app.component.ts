e48e524cbda72008a3a5bff9d289debf79ff2e94
Third <ignaciotabug36@gmail.com>
2024-07-01 16:29:30 +0800

    Login feature in Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'HRIS_login-page';
+  passwordHidden: boolean = true;
+
+  togglePasswordVisibility(): void {
+    this.passwordHidden = !this.passwordHidden;
+    const passwordField = document.getElementById('password') as HTMLInputElement;
+    passwordField.type = this.passwordHidden ? 'password' : 'text';
+  }
+}
