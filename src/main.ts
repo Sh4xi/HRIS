6fff123e744ce88693fb64bb9a3d966e17774b11
TechnoLophie <jhielofrozt@gmail.com>
2024-07-03 12:57:26 +0100

    12-13-14-16 from Jhielo/System to Quanby/HRIS

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { appConfig } from './app-component/app.config';
+import { AppComponent } from './app-component/app.component';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
+import { routes } from './app-component/app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
-    provideRouter(routes)
+    provideRouter(routes), provideAnimationsAsync()
   ]
 })
 .catch((err) => console.error(err));
+
