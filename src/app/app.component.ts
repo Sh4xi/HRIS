daa72ee2c37e4f9a313e02cc85bab2c2f7c3b944
Third <ignaciotabug36@gmail.com>
2024-07-03 11:21:09 +0800

    Updated login, dashboard, and udermanagement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserManagementComponent } from './user-management/user-management.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, DashboardComponent, UserManagementComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'HRIS_login-page';
-  passwordHidden: boolean = true;
-
-  togglePasswordVisibility(): void {
-    this.passwordHidden = !this.passwordHidden;
-    const passwordField = document.getElementById('password') as HTMLInputElement;
-    passwordField.type = this.passwordHidden ? 'password' : 'text';
-  }
 }
