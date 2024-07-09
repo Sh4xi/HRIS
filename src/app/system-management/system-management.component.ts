ec74bbea047b5bbb901203c8a8aed93db9de6437
alexnadsat <leanjedfreydimson.deduque@bicol-u.edu.ph>
2024-07-09 08:47:23 +0800

    added new component system-management

---
+++ src/app/system-management/system-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './system-management.component.html',
+  styleUrl: './system-management.component.css'
+})
+export class SystemManagementComponent {
+
+}
