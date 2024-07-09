30279127e1d130746031316ef25387c982f2a853
alexnadsat <leanjedfreydimson.deduque@bicol-u.edu.ph>
2024-07-09 15:20:04 +0800

    added parameter types and working popup when creating new parameter for system management

--- src/app/system-management/system-management.component.ts
+++ src/app/system-management/system-management.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-system-management',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './system-management.component.html',
   styleUrl: './system-management.component.css'
 })
 export class SystemManagementComponent {
+  showPopup = false;
+  parameterName: string = '';
+  selectedType: string = '';
+  types: string[] = ['Holiday', 'OT Type', 'Schedule', 'Leave'];
+  holidayDate: string = ''; // New property for holiday date
 
+  openPopup() {
+    this.showPopup = true;
+  }
+
+  closePopup() {
+    this.showPopup = false;
+    this.resetForm();
+  }
+
+  saveParameter() {
+    console.log('Saving parameter:', this.parameterName, this.selectedType);
+    if (this.selectedType === 'Holiday') {
+      console.log('Holiday date:', this.holidayDate);
+    }
+    this.closePopup();
+  }
+
+  resetForm() {
+    this.parameterName = '';
+    this.selectedType = '';
+    this.holidayDate = '';
+  }
 }
---
+++ src/app/system-management/system-management.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SystemManagementComponent } from './system-management.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    UserManagementModule
+  ]
+})
+export class UserManagementModule { }
