e0a98206585fc6ca5bbb814a13b3cde9c1404663
gelogrammer <angelojohnse.calleja@bicol-u.edu.ph>
2024-07-08 16:38:30 -0800

    Save work before cleaning

--- src/app/Supabase/supabase.service.ts
+++ src/app/Supabase/supabase.service.ts
@@ -23,7 +23,7 @@ export class SupabaseService {
         first_name: employee.firstname,
         mid_name: employee.midname,
         surname: employee.surname,
-        password: employee.password, // Hash the password before storing // eto yung sa password sa pag display sa supabase
+        password: employee.passwrod, // Hash the password before storing
         department: employee.department,
         position: employee.position,
         types: employee.type
